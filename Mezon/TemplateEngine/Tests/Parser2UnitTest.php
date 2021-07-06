d0f218ec0efa3b4dc60eee434ab3c1f01d6fe29a
gdever <alexey@dodonov.pro>
2021-07-06 09:05:49 +0300

    feature/Unit-tests preparations for Parser 2

---
+++ Mezon/TemplateEngine/Tests/Parser2UnitTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Mezon\TemplateEngine\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Mezon\TemplateEngine\Parser;
+use Mezon\TemplateEngine\Parser2;
+
+/**
+ *
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+class Parser2UnitTest extends TestCase
+{
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \PHPUnit\Framework\TestCase::setUpBeforeClass()
+     */
+    public static function setUpBeforeClass(): void
+    {
+        TemplateEngine::$parser = Parser2::class;
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        TemplateEngine::$parser = Parser::class;
+    }
+}
